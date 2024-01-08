17a27d0cad77f1535af0ac27dd089c31645b0643
JohnyBoy2128 <cannonofficial2128@icloud.com>
2024-01-08 11:16:18 -0500

    Lots of new things! Added vision folder for EOCV-Sim Added MeepMeep for testing TODO made in Arc

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Centerstage/FirstAuto.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.Centerstage;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous(name = "CS: First Auto", group = "Centerstage")
+public class FirstAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        TrajectorySequence testTrack = drive.trajectorySequenceBuilder(new Pose2d(24.00, -48.00, Math.toRadians(90.00)))
+                .splineTo(new Vector2d(48.00, -0.00), Math.toRadians(90.00))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(31.00, 48.00), Math.toRadians(180.00))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(12.00, -0.00), Math.toRadians(-90.00))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(36.00, -60.00), Math.toRadians(0.00))
+                .waitSeconds(1)
+                .build();
+
+
+
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectorySequence(testTrack);
+    }
+
+}
