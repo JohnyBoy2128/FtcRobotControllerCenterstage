61d063e74a562ad53904cfe8fe4fa35a4bdf835a
JohnyBoy2128 <cannonofficial2128@icloud.com>
2024-01-14 09:32:29 -0500

    BlueLeftAuto fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Centerstage/BlueRightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Centerstage/BlueRightAuto.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.Centerstage;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -8,21 +9,26 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.drive.ScoringMechanism;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-@Autonomous(name = "BlueLeftAuto", group = "Centerstage")
-public class BlueLeftAuto extends LinearOpMode {
+@Autonomous(name = "BlueRightAuto", group = "Centerstage")
+public class BlueRightAuto extends LinearOpMode {
 
     private ScoringMechanism mechanism = new ScoringMechanism();
+
     @Override
     public void runOpMode() {
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        mechanism.closeRightClaw();
         mechanism.closeLeftClaw();
+        mechanism.closeRightClaw();
 
-        TrajectorySequence testTrack = drive.trajectorySequenceBuilder(new Pose2d(12, -62.75, Math.toRadians(90)))
-                .lineToSplineHeading(new Pose2d(54, -62.75, Math.toRadians(90.00)))
-                .build();
+        TrajectorySequence testTrack = drive.trajectorySequenceBuilder(new Pose2d(-36.00, 62.75, Math.toRadians(270.00)))
+                .lineTo(new Vector2d(-36.04, 20.98))
+                .lineTo(new Vector2d(-36.04, 29.08))
+                .lineTo(new Vector2d(-49.16, 32.80))
+                .lineTo(new Vector2d(-49.00, 13.20))
+                .lineTo(new Vector2d(58.72, 13.04))
+                .build());
 
 
 
